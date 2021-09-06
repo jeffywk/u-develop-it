345518acd9fb37102b049848132815404222c1c2
Apo11o <jeffknoespel@gmail.com>
2021-09-06 13:42:35 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
