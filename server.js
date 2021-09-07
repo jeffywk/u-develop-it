ab6708d7d5a55547c133f05b30b5f61ef4a3c94b
Apo11o <jeffknoespel@gmail.com>
2021-09-06 21:55:07 -0500

    deleted password

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'Autofan911',
+      password: '',
       database: 'election'
     },
     console.log('Connected to the election database.')
